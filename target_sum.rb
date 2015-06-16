ef96aa5b08eb3ec96472749a6c85c06b934bea20
Vladimir Sobolev <v11111000001@gmail.com>
2015-06-16 13:25:37 +0300

    Add an alternate solution to a target_sum problem.

--- target_sum.rb
+++ target_sum.rb
@@ -1,6 +1,7 @@
 # Given an integer and array of integers. Determine whether any 2 integers in the array sums to that integer.
-
 class Array
+
+  # memory efficient
   def has_pair_that_sums_to(target_sum)
     self.sort!
     
@@ -9,8 +10,28 @@ class Array
 
       found = bin_search(summand, i + 1, self.length - 1)
 
-      # return "#{n} + #{self[found]} = #{target_sum}" if found
       return true if found
+    end
+
+    false
+  end
+
+  # time efficient
+  def alternate_has_pair_that_sums_to(target_sum)
+    self.sort!
+
+    hash = {}
+    
+    self.each_with_index do |n, i|
+      summand = target_sum - n
+
+      if hash.has_key? summand
+        return true
+      else
+        hash[n] = true
+      end
+
+      false
     end
 
     false
@@ -37,3 +58,9 @@ puts [-1,0,1].has_pair_that_sums_to 0
 puts [-5,0,1,3,10].has_pair_that_sums_to 5
 puts [0,1,2,3].has_pair_that_sums_to 0
 puts [0,1,2,3].has_pair_that_sums_to 10
+
+puts '========'
+puts [-1,0,1].alternate_has_pair_that_sums_to 0
+puts [-5,0,1,3,10].alternate_has_pair_that_sums_to 5
+puts [0,1,2,3].alternate_has_pair_that_sums_to 0
+puts [0,1,2,3].alternate_has_pair_that_sums_to 10
